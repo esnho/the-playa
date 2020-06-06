6d0612bd690159721e44f6dbf05918220f9686dc
esnho <esnhoesnho@gmail.com>
2020-06-07 00:53:02 +0200

    Set output to howler format

--- convertAudio.sh
+++ convertAudio.sh
@@ -8,4 +8,4 @@ while read -d $'\0' file; do
     FILES="${FILES} $file"
 done < <( find ${mydir} -type f \( ! -iname ".*" ! -iname "*.html" \) -print0 )
 
-audiosprite --path audio --output audio/$2 ${FILES}
+audiosprite --path audio --format howler2 --output audio/$2 ${FILES}
