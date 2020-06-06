401d0544780c958b3d6b6c3a0d45722192f07229
esnho <esnhoesnho@gmail.com>
2020-06-06 19:09:49 +0200

    Add command line parameters

--- convertAudio.sh
+++ convertAudio.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-mydir=./SAMPLESWAP
+mydir=$1
 
 FILES=""
 
 while read -d $'\0' file; do
     FILES="${FILES} $file"
-    echo "ProcessingTwo $file $FILES"
 done < <( find ${mydir} -type f \( ! -iname ".*" ! -iname "*.html" \) -print0 )
 
-audiosprite --output theplayainput${FILES}
+audiosprite --output $2 ${FILES}
